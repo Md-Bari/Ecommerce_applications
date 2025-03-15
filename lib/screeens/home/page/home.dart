2a77b119ba21b6c180c054cd623792de629c5a30
md-Bari <bari15-5598@diu.edu.bd>
2025-03-15 10:39:43 +0600

    fixed app bar

--- lib/screeens/home/page/home.dart
+++ lib/screeens/home/page/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
+import '../../../common/widgets/app_bar.dart';
 import '../../../utils/constants/colors.dart';
 
 class Home extends StatelessWidget {
@@ -7,28 +8,9 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Mega Shop",
-          style:TextStyle(
-          fontFamily: "DMSans",
-          fontWeight: FontWeight.bold,
-          color: FColors.oceanBlue,
-          ),
-        ),
-        centerTitle: true,
-        actions: [
-          Container(
-            padding: EdgeInsets.all(10),
-            child: Icon(Iconsax.notification_bing_outline),
-            ),
-            Container(
-            padding: EdgeInsets.all(10),
-            child: Icon(Icons.shopping_cart),
-            ),
-        ],
-      ),
-    );
+    return Scaffold(appBar: FAppBar(
+      title: "New Title",
+      firsticon: Icon(icon),
+    ));
   }
 }

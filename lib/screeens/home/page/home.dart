726fba60c5346d8020f1ccc6fe0ee30cb64a9ee3
md-Bari <bari15-5598@diu.edu.bd>
2025-03-15 10:44:27 +0600

    seperated app bar

--- lib/screeens/home/page/home.dart
+++ lib/screeens/home/page/home.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:icons_plus/icons_plus.dart';
-import '../../../common/widgets/app_bar.dart';
-import '../../../utils/constants/colors.dart';
+import '../../../common/widget/appbar.dart';
+
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: FAppBar(
-      title: "New Title",
-      firsticon: Icon(icon),
-    ));
+    return Scaffold(appBar: FAppBar(title: "New Title"));
   }
 }
